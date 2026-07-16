0a1fa225d45f909cd4e0a2b6ce73b7890543e350
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-07-16 20:03:45 +0530

    create product validation

---
+++ src/product/product-router.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import { ProductController } from "./product-controller"
+import authenticate from "../common/middlewares/authenticate"
+import { canAccess } from "../common/middlewares/canAccess"
+import { Roles } from "../common/constants"
+import { asyncWrapper } from "../common/utils/async-wrapper"
+import createProductValidator from "./create-product-validator"
+
+const router = express.Router()
+
+const productController = new ProductController()
+
+router.post("/", authenticate, canAccess([Roles.ADMIN, Roles.MANAGER]), createProductValidator ,asyncWrapper(productController.create))
+
+export default router
