72bdf94cff13b6109aac06c5b0a74ea152d9a9c0
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-07-22 17:48:16 +0530

    add cors

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,17 @@ import categoryRouter from "./category/category-router";
 import productRouter from "./product/product-router";
 import toppingRouter from "./topping/topping-router";
 import cookieParser from "cookie-parser"
+import cors from "cors"
+import config from "config"
 
 const app = express();
+
+app.use(
+    cors({
+        origin: config.get("frontend.ADMIN_DASHBOARD_BASE_URL"),
+        credentials: true,
+    }),
+);
 
 app.use(express.json())
 app.use(cookieParser())
